9f946eff436faa168dcf9daacc5076ab9fb66470
jsowers13 <jsowers13@gmail.com>
2022-08-23 19:51:46 +0000

    created new render of view work order page

--- src/front/js/component/viewworkorder.js
+++ src/front/js/component/viewworkorder.js
@@ -14,6 +14,15 @@ export const ViewWorkOrder = () => {
     return true;
   };
 
+  const EntryBar = (props) => {
+    return (
+      <div className="row border-bottom">
+        <div className="col-3">{props.entryname}</div>
+        <div className="col-6">{props.entrydata}</div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     async function fetchWorkOrder(id) {
       const fetchData = await actions.getWorkOrderByID(id);
@@ -27,7 +36,7 @@ export const ViewWorkOrder = () => {
     return <h1>Loading Work Order Data...</h1>;
   } else {
     return (
-      <div className="col text-center">
+      <div className="container">
         {/* <header>
           <span
             type="button"
@@ -46,8 +55,60 @@ export const ViewWorkOrder = () => {
           rightlink="/"
           righttitle="Edit"
         />
-        {currentWorkOrder.title}
-        {currentWorkOrder.description}
+        <p>Call Date</p>
+        <EntryBar entryname="Property" entrydata="PlaceHolder" />
+        <EntryBar entryname="Property Name" entrydata="PlaceHolder" />
+        <EntryBar entryname="Issue" entrydata={currentWorkOrder.title} />
+        <EntryBar entryname="Category" entrydata={currentWorkOrder.category} />
+        <EntryBar
+          entryname="Description"
+          entrydata={currentWorkOrder.description}
+        />
+        <EntryBar entryname="Status" entrydata={currentWorkOrder.status} />
+        <EntryBar
+          entryname="Entry Allowed?"
+          entrydata={
+            currentWorkOrder.entry_allowed ? (
+              <i className="fa-solid fa-circle-check text-success mt-3"></i>
+            ) : (
+              <i className="fa-solid fa-circle-xmark text-danger mt-3"></i>
+            )
+          }
+        />
+
+        <div class=" row border-bottom px-2">
+          <label htmlFor="access-notes-field" class="form-label">
+            Access Notes:
+          </label>
+          <textarea class="rounded" readOnly id="access-notes-field" rows="3">
+            {currentWorkOrder.access_notes}
+          </textarea>
+          <hr />
+        </div>
+        <EntryBar
+          entryname="Bill to Customer?"
+          entrydata={
+            currentWorkOrder.bill_to_customer ? (
+              <i className="fa-solid fa-circle-check text-success mt-3"></i>
+            ) : (
+              <i className="fa-solid fa-circle-xmark text-danger mt-3"></i>
+            )
+          }
+        />
+        {/* <div className="row border-bottom">
+          <p>Maintenance Notes:</p>
+          <textarea className="m-2">
+            {currentWorkOrder.maintenance_notes}
+          </textarea>
+        </div> */}
+        <div class=" row border-bottom px-2">
+          <label htmlFor="maint-notes-field" class="form-label">
+            Maintenance Notes:
+          </label>
+          <textarea class="rounded" readOnly id="maint-notes-field" rows="3">
+            {currentWorkOrder.maintenance_notes}
+          </textarea>
+        </div>
 
         <Link to="/">
           <button className="btn btn-primary w-100">Main Menu</button>
