85bdb6c9c2a0211a57fabd501f660fa26b031634
jsowers13 <jsowers13@gmail.com>
2022-08-30 01:05:05 +0000

    added filter by dropdown menus

--- src/front/js/component/adminviewworkorders.js
+++ src/front/js/component/adminviewworkorders.js
@@ -24,6 +24,103 @@ export const AdminViewWorkOrders = () => {
     return (
       <div className="container text-center">
         <h1>Admin View</h1>
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="filterByDropdown"
+            data-bs-toggle="dropdown"
+            data-bs-auto-close="outside"
+            aria-expanded="false"
+          >
+            Filter By
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="filterByDropdown">
+            <li className="dropdown-item">
+              <div className="dropend">
+                <span
+                  className="dropdown-toggle"
+                  type="button"
+                  id="categoryDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Category
+                </span>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="categoryDropdown"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Electric
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Plumbing
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      HVAC
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Carpentry
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="dropdown-item">
+              <div className="dropend">
+                <span
+                  className="dropdown-toggle"
+                  type="button"
+                  id="statusDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Status
+                </span>
+                <ul className="dropdown-menu" aria-labelledby="statusDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Call
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Cancelled
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Scheduled
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      On Hold
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      In Progress
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Completed
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
         <table className="table table-striped" id="workOrderTable">
           <thead>
             <tr>
